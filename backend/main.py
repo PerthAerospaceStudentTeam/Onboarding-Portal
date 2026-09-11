2ddf37e954c39572e8402977ce9e821f4ec5f4ea
uni-ro <thomas.unipan@student.curtin.edu.au>
2026-09-11 10:28:44 +0800

    Added HTTPException import to fastapi

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-
 from config import get_settings
 
 settings = get_settings()
